0297f7c0695e10a4d762178e2effe56cb813b790
Wess Cope <wcope@me.com>
2019-04-30 12:01:51 -0400

    Adds list with list items, including breadcrumb and pagination setup

--- app/javascript/components/Project/list.js
+++ app/javascript/components/Project/list.js
@@ -7,12 +7,63 @@
  */
 
 import React from 'react';
+import { Media } from 'bloomer/lib/components/Media/Media';
+import { MediaLeft } from 'bloomer/lib/components/Media/MediaLeft';
+import { MediaContent } from 'bloomer/lib/components/Media/MediaContent';
+import { MediaRight } from 'bloomer/lib/components/Media/MediaRight';
+import { ProjectListItem } from './list_item';
+import { Container } from 'bloomer/lib/layout/Container';
+import { Breadcrumb } from 'bloomer/lib/components/Breadcrumb/Breadcrumb';
+import { Pagination } from 'bloomer/lib/components/Pagination/Pagination';
+import { BreadcrumbItem } from 'bloomer/lib/components/Breadcrumb/BreadcrumbItem';
+import { PageControl } from 'bloomer/lib/components/Pagination/PageControl';
+import { PageList } from 'bloomer/lib/components/Pagination/PageList';
+import { Page } from 'bloomer/lib/components/Pagination/Page';
+import { PageLink } from 'bloomer/lib/components/Pagination/PageLink';
+import { PageEllipsis } from 'bloomer';
 
 export class ProjectList extends React.Component {
   render() {
     return (
-      <div>
-        <p>LIIIISSSTTT</p>
+      <div className="section">
+        <Container>
+          <Breadcrumb>
+            <ul>
+              <BreadcrumbItem className="title is-4" isActive>
+                <a href="javascript:;">Projects</a>
+              </BreadcrumbItem>
+            </ul>
+          </Breadcrumb>
+        </Container>
+        
+        <hr/>
+        <br/>
+
+        <Container>
+          <ProjectListItem />
+          <ProjectListItem />
+          <ProjectListItem />
+          <ProjectListItem />
+        </Container>
+
+        <br/>
+
+        <Container>
+          <Pagination isAlign="centered">
+            <PageControl isPrevious>Previous</PageControl>
+            <PageControl isNext>Next</PageControl>
+
+            <PageList>
+              <Page><PageLink>1</PageLink></Page>
+              <Page><PageEllipsis/></Page>
+              <Page><PageLink>21</PageLink></Page>
+              <Page><PageLink>22</PageLink></Page>
+              <Page><PageLink>23</PageLink></Page>
+              <Page><PageEllipsis/></Page>
+              <Page><PageLink>60</PageLink></Page>
+            </PageList>
+          </Pagination>
+        </Container>
       </div>
     )
   }
