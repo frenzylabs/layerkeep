bfb04ba4ac0eeb5442ee455c7f851eeef3013275
Kevin Musselman <kmussel@gmail.com>
2019-05-23 14:34:34 -0400

    Update remote messaging

--- app/javascript/RemoteMessage.js
+++ app/javascript/RemoteMessage.js
@@ -8,20 +8,19 @@
 
 import React              from 'react'
 import { connect }        from 'react-redux';
-import { BrowserRouter }  from 'react-router-dom'
 
 import UserHandler from './handlers/user_handler';
 import StompSocket from './StompSocket';
 
 import { toast }          from 'react-toastify';
 
-class Not extends React.Component {
+class RemoteMessage extends React.Component {
   constructor(props) {
     super(props);
     var topics = []
     if (currentUser) {
       topics = ['/topic/LK.#', '/topic/'+currentUser.username]
-    }
+    }    
     this.state = {token: null, topics: topics}
 
     this.onConnect  = this.onConnect.bind(this);
@@ -29,17 +28,16 @@ class Not extends React.Component {
     this.onMessage  = this.onMessage.bind(this);
     this.authToken  = this.authToken.bind(this);
 
-    window.not = this;
-
-    
     this.authToken()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
   }
 
 
   authToken() {
     UserHandler.authToken()
     .then((response) => {
-      console.log(response)
       this.setState( {token: response.data.token });
     })
     .catch((error) => {
@@ -47,12 +45,8 @@ class Not extends React.Component {
     });
   }
 
-  // sendMessage = (msg) => {
-  //   this.clientRef.sendMessage('/topics/all', msg);
-  // }
-
   onConnect() {
-    console.log("CONNECTED NOTI")
+    console.log("Remote Notifications Connected")
   }
 
   onError(err) {
@@ -61,27 +55,20 @@ class Not extends React.Component {
 
   onMessage(msg) {
     console.log("INSIDE NOT MSG, ", msg);    
-    toast("MSG= " + msg.body );
+    toast("MSG= " + msg.body, { autoClose: 150000 } );
   }
 
-  renderConn() {
-    
-  }
 
   render() {
     if (!(currentUser && currentUser.username && this.state.token)) {
-      console.log("INSIDE RENDER EMPTY DIV")
       return (<div></div>)
-    } else {
-      console.log("RENDER WEBSOCKET")
     }
     return (
       <div>
         <StompSocket user={currentUser.username} password={this.state.token} topics={this.state.topics}
             onConnect={this.onConnect}
             onError={this.onError}
-            onMessage={this.onMessage}
-            ref={ (client) => { this.clientRef = client }} />
+            onMessage={this.onMessage} />
       </div>
     );
   }
@@ -98,4 +85,4 @@ function mapDispatchToProps() {
   return {};
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Not);
+export default connect(mapStateToProps, mapDispatchToProps)(RemoteMessage);
