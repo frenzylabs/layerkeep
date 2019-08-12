1c3745067193549da1b6d26a9456ae9ba5341beb
Wess Cope <wcope@me.com>
2019-08-12 12:27:41 -0400

    Creating token and card with some spinnners involved

--- app/javascript/components/Modal/stripe.js
+++ app/javascript/components/Modal/stripe.js
@@ -6,29 +6,81 @@
 //  Copyright 2019 FrenzyLabs,llc.
 //
 
-import React            from 'react';
-import {StripeProvider, Elements} from 'react-stripe-elements';
-
-import BillingForm  from '../Settings/billing/form'
+import React                      from 'react'
+import {StripeProvider, Elements} from 'react-stripe-elements'
+import UserHandler                from '../../handlers/user_handler'
+import BillingForm                from '../Settings/billing/form'
+import SpinnerModal               from './spinner'
 
 export default class StripeModal extends React.Component {
   title = "Update Billing";
   
   constructor(props) {
     super(props);
+
+    this.state = {
+      isCreating: false,
+      error:      null
+    }
+  }
+
+  async handleSubmit(token) {
+
+    console.log("token: ", token)
+
+    if(token.error) {
+      console.error('Payload error', token)
+      return
+    }
+
+    this.setState({
+      ...this.state,
+      isCreating : true
+    })
+
+    UserHandler
+    .createCard(window.currentUser.username, {'source_token': token.id})
+    .then((res) => {
+      this.props.dismissAction()
+    })
+    .catch((err) => {
+      this.setState({
+        ...this.state,
+        error:      err,
+        isCreating: false
+      })
+    })
   }
 
   render() {
+    if(this.state.isCreating) {
+      return (
+        <SpinnerModal caption="Saving..."/>
+      )
+    }
+
     return (
       <div className="card">
         <div className="card-header">
           <p className="card-header-title">Billing Info.</p>
         </div>
 
+        { this.state.error && (
+          <article class="message is-danger">
+            <div class="message-body">
+              {this.state.error}
+            </div>
+          </article>
+        )}
+
         <div className="card-content">
           <StripeProvider stripe={this.props.stripe}>
             <Elements>
-              <BillingForm/>
+              <BillingForm 
+                handleSubmit={this.handleSubmit.bind(this)}
+                dismissAction={this.props.dismissAction} 
+                stripe={this.props.stripe}
+              />
             </Elements>
           </StripeProvider>
 
--- app/javascript/components/Settings/billing/form/index.js
+++ app/javascript/components/Settings/billing/form/index.js
@@ -10,28 +10,30 @@ import React from 'react'
 
 import {
   injectStripe,
-  CardElement,
-  PostalCodeElement
+  CardElement
 } from 'react-stripe-elements';
 
 class BillingForm extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleSubmit(ev) {
+  async submitAction(ev) {
     ev.preventDefault()
 
-    if(!this.props.stripe) { return }
+    if(!this.props.stripe) {
+      console.error("No stripe property")
+      return
+    }
 
-    // const stripe = this.props.stripe 
+    let response = await this.props.stripe.createToken({
+      type: 'card',
+      name: window.currentUser.username
+    })
+
+    await this.props.handleSubmit(response)
+
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.submitAction.bind(this)}>
         <div className="field">
           <div className="control">
             <CardElement/>
@@ -41,15 +43,25 @@ class BillingForm extends React.Component {
         <br/>
         <div className="field">
           <div className="control columns">
-            
+            <div className="column is-8">
+              <div style={{overflow: 'hidden', paddingTop: '6px'}}>
+                <p style={{fontSize: '12px', textAlign: 'right', overflow: 'hidden', lineHeight: '200%'}}>
+                  Powered by &nbsp;
+                  <span className="icon is-medium" style={{display: 'block', float: 'right'}}>
+                    <a href="https://stripe.com" target="_blank"><i className="fab fa-stripe fa-2x"></i></a>
+                  </span>
+                </p>
+              </div>
+            </div>
+
             <div className="buttons column">
-              <button class="button is-light">
+              <button className="button is-light" onClick={this.props.dismissAction}>
                 <span>Cancel</span>
               </button>
 
-              <button class="button is-success">
-                <span class="icon is-small">
-                  <i class="fas fa-check"></i>
+              <button className="button is-success">
+                <span className="icon is-small">
+                  <i className="fas fa-check"></i>
                 </span>
                 <span>Save</span>
               </button>
