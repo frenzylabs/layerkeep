1e100881d658ddc2bcff0e8f7ed0f42fb12da2fd
Kevin Musselman <kmussel@gmail.com>
2019-12-30 15:24:31 -0500

    render print description

--- app/javascript/components/Prints/list_item.js
+++ app/javascript/components/Prints/list_item.js
@@ -39,6 +39,21 @@ export class PrintListItem extends React.Component {
       <p className="title is-5" style={{lineHeight: "154%", marginBottom: "0"}}>{itematt.name}</p>
     )
   }
+
+  renderDescription() {
+    if (this.props.item.attributes.description) {
+      var d = this.props.item.attributes.description
+      if (d.length > 216) {
+        d = d.substring(0, 216)
+        var last = d.lastIndexOf(" ")
+        return d.substring(0, last) + "..."
+      }
+      return d
+      
+    }
+    return ""
+  }
+
   render() {
     return (
       <Link to={this.props.path} className="box" data-id={this.props.item.id} key={this.props.item.id} >
@@ -49,7 +64,7 @@ export class PrintListItem extends React.Component {
 
           <MediaContent>
             {this.renderTitle()}
-            <p className="has-text-grey">{this.props.item.attributes.description || ''}</p>
+            <p className="has-text-grey">{this.renderDescription()}</p>
           </MediaContent>
 
           <MediaRight>
