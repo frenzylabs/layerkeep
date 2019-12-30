575e8f75a86b22a8fedc3efa07b4e0bfeab35127
Kevin Musselman <kmussel@gmail.com>
2019-12-30 15:51:35 -0500

    truncate description of slice/printer in list view

--- app/javascript/components/Printers/list_item.js
+++ app/javascript/components/Printers/list_item.js
@@ -30,6 +30,20 @@ export class PrinterListItem extends React.Component {
     }
   }
 
+  renderDescription() {
+    if (this.props.item.attributes.description) {
+      var d = this.props.item.attributes.description
+      if (d.length > 216) {
+        d = d.substring(0, 216)
+        var last = d.lastIndexOf(" ")
+        return d.substring(0, last) + "..."
+      }
+      return d
+      
+    }
+    return ""
+  }
+
   render() {
     return (
       <Link to={this.props.path} className="box" data-id={this.props.item.id} key={this.props.item.id} >
@@ -40,7 +54,7 @@ export class PrinterListItem extends React.Component {
 
           <MediaContent>
             <p className="title is-5" style={{lineHeight: "154%", marginBottom: "0"}}>{this.props.item.attributes.name}</p>
-            <p className="has-text-grey">{this.props.item.attributes.description || ''}</p>
+            <p className="has-text-grey">{this.renderDescription()}</p>
           </MediaContent>
 
           <MediaRight>
